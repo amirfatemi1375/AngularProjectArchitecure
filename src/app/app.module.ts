dfc0b32b5df0b205918998e2735987b59a6b603c
amir fatemi <amirhosseinfatemi559@gmail.com>
2022-12-17 13:08:09 -0800

    [feat] project architecture sample utility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainLayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { InputComponent } from './components/input/input.component';
+import { SamplePipe } from './pipes/sample.pipe';
+import { DropdownDirective } from './directives/dropdown.directive';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [InputComponent, SamplePipe, DropdownDirective],
+  imports: [CommonModule],
+  exports: [InputComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
